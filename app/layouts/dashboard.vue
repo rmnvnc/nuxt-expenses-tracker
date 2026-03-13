ef52d22b1386fe5c33837542e5fe7b072630acd0
rmnvnc <81642663+rmnvnc@users.noreply.github.com>
2026-03-13 16:34:22 +0100

    Zabudnuty layout

---
+++ app/layouts/dashboard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const { profile } = useUser()
+const isReady = computed(() => profile.value !== null)
+</script>
+
+<template>
+    <AppHeader />
+    <UMain>
+        <UContainer>
+            <div
+                v-if="!isReady"
+                class="flex items-center justify-center min-h-64"
+            >
+                <UIcon
+                    name="ph:circle-notch-bold"
+                    class="w-10 h-10 animate-spin text-muted2"
+                />
+            </div>
+            <slot v-else />
+        </UContainer>
+    </UMain>
+</template>
